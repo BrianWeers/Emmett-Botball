f59de14842cd052c9e629a7427f6974d27791242
BrianWeers <Brian.Weers@gmail.com>
2019-07-09 15:34:27 +0000

    Added new folder and Hello World File

---
+++ Christina Folder/Hello World.c.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <kipr/botball.h>
+
+
+int main(void)
+{
+    printf("Begin Deni");
+    Deni();
+    printf("Begin Hayden");
+    Hayden();
+    printf("Begin Caleb");
+    Caleb();
+    //Brian Change
+    
+}
+
+//all Calebs code goes here
+int Caleb() {
+    
+}
+
+
+//all Haydens code goes here
+int Hayden(){
+    
+}
+
+//all 
+int Deni(){
+    
+}
+void Hopper(){
+    printf("test syntax")
+}
